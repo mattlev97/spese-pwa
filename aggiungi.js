5166da646806e852ecd92561738c3b7caf7aede7
mattlev97 <matteoenrico.levati@gmail.com>
2025-08-09 16:45:33 +0200

    Update aggiungi.js

--- aggiungi.js
+++ aggiungi.js
@@ -4,14 +4,14 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const nuovaSpesa = {
-    supermercato: document.getElementById("supermercato").value,
-    tipologia: document.getElementById("tipologia").value,
-    nome: document.getElementById("nome").value,
-    prezzo: parseFloat(document.getElementById("prezzo").value),
-    prezzoKg: parseFloat(document.getElementById("prezzoKg").value) || null,
-    note: document.getElementById("note").value,
-    data: new Date().toISOString()
-  };
+  data: new Date().toISOString(),
+  supermercato: document.getElementById("supermercato").value,
+  tipologia: document.getElementById("tipologia").value,
+  nome: document.getElementById("nome").value,
+  prezzo: parseFloat(document.getElementById("prezzo").value),
+  prezzoKg: parseFloat(document.getElementById("prezzoKg").value) || null,
+  note: document.getElementById("note").value
+};
 
   const spese = JSON.parse(localStorage.getItem("spese")) || [];
   spese.push(nuovaSpesa);
