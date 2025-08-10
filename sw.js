32cf6e5292a349443cf28dc7672db9541699d6c8
mattlev97 <matteoenrico.levati@gmail.com>
2025-08-11 01:08:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,29 @@
-const CACHE_NAME = "spesa-cache-v2";
+// sw.js
+const CACHE_NAME = 'spese-app-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./aggiungi.html",
-  "./archivio.html",
-  "./style.css",
-  "./aggiungi.js",
-  "./archivio.js",
-  "./dashboard.js"
+    '/',
+    '/index.html',
+    '/aggiungi.html',
+    '/archivio.html',
+    '/style.css',
+    '/script.js',
+    'https://cdn.jsdelivr.net/npm/chart.js'
 ];
 
-// Installazione del service worker e cache iniziale
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-// Attivazione e pulizia cache vecchia
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      )
-    )
-  );
-});
-
-// Recupero file dalla cache o dalla rete
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request).then((res) => {
-          return caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, res.clone());
-            return res;
-          });
-        })
-      );
-    })
-  );
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
